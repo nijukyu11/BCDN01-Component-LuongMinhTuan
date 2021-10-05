5c0635bacc64d8680c21302b2ce512383972219f
Tuan <lminhtuan2911@gmail.com>
2021-10-05 20:52:22 +0700

    done

---
+++ src/Components/BaiTapThucHanhLayout/Item.js
@@ -0,0 +1,55 @@
+import React, { Component } from 'react'
+
+export default class Item extends Component {
+    render() {
+        return (
+            <div className="pt-4">
+                <div className="container px-lg-5">
+                    <div className="row gx-lg-5">
+                        <div className="col-3 mb-5">
+                            <div className="card text-center">
+                                <img src="..." className="card-img-top" alt="..." />
+                                <div className="card-body">
+                                    <h5 className="card-title">Card title</h5>
+                                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                    <a href="#" className="btn btn-primary">Go somewhere</a>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-3 mb-5">
+                            <div className="card text-center">
+                                <img src="..." className="card-img-top" alt="..." />
+                                <div className="card-body">
+                                    <h5 className="card-title">Card title</h5>
+                                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                    <a href="#" className="btn btn-primary">Go somewhere</a>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-3 mb-5">
+                            <div className="card text-center">
+                                <img src="..." className="card-img-top" alt="..." />
+                                <div className="card-body">
+                                    <h5 className="card-title">Card title</h5>
+                                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                    <a href="#" className="btn btn-primary">Go somewhere</a>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-3 mb-5">
+                            <div className="card text-center">
+                                <img src="..." className="card-img-top" alt="..." />
+                                <div className="card-body">
+                                    <h5 className="card-title">Card title</h5>
+                                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                    <a href="#" className="btn btn-primary">Go somewhere</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        );
+    }
+}
